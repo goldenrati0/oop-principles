84844f90d0b66faf0fe02d51677af39418962cdc
Tahmid Choyon <tahmid.hossain@newscred.com>
2021-08-14 09:37:43 +0600

    Add new class for items with free items

--- pizza-shop/crust.py
+++ pizza-shop/crust.py
@@ -1,16 +1,13 @@
-from typing import List
-
-from item import ItemInterface
-from free_item import FreeItemInterface
-from toppings import Pepperoni
+from item import Item
+from topping import FreePepperoni
 
 
-class PizzaCrustInterface(ItemInterface):  # IS-A Item
+class PizzaCrust(Item):  # IS-A Item
     def __init__(self) -> None:
         super().__init__()
 
 
-class ThinCrust(PizzaCrustInterface):  # IS-A Crust
+class ThinCrust(PizzaCrust):  # IS-A Crust
     def __init__(self) -> None:
         super().__init__()
 
@@ -18,11 +15,23 @@ class ThinCrust(PizzaCrustInterface):  # IS-A Crust
         self.set_price(2.0)
 
 
-class ThickCrust(PizzaCrustInterface):  # IS-A Crust
+class ThickCrust(PizzaCrust):  # IS-A Crust
     def __init__(self) -> None:
         super().__init__()
 
         self.set_name('Thick')
         self.set_price(3.0)
 
-        self.add_free_item(FreeItemInterface(Pepperoni()))
+
+class ThickCrustWithFreeItems(ThickCrust):  # IS-A ThickCrust
+    """
+    Created this class just to make things simpler.
+    ThickCrustWithFreeItems might look unnecessary becuase Item HAS-A FreeItem, so could have
+    added those FreeItem(s) in ThickCrust class. In that case, we needed to add a bool in the
+    constructor should_add_free_items. I did not want to add this bool variable, and hence
+    the new class.
+    """
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.add_free_item(FreePepperoni())
